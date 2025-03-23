a2775b6a64732810945ac4bcce433ecaf42e01df
Gabriel Oliveira <gabrielgbo00@gmail.com>
2025-03-23 20:39:22 -0300

    Initial commit

---
+++ FinancialAdvisorChatbot/src/main/java/com/gabrieloliveira/financial_advisor_backend/FinancialAdvisorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gabrieloliveira.financial_advisor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAdvisorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAdvisorBackendApplication.class, args);
+	}
+
+}
---
+++ FinancialAdvisorChatbot/src/test/java/com/gabrieloliveira/financial_advisor_backend/FinancialAdvisorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieloliveira.financial_advisor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAdvisorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
